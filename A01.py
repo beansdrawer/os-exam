6a79ba956a33e06196eb71a6df84d1fefff38c53
beansdrawer <64694081+beansdrawer@users.noreply.github.com>
2024-02-28 12:07:19 +0900

    Create A01.py

---
+++ A01.py
@@ -0,0 +1,14 @@
+# 10진수
+decimal_number = 118
+
+# 10진수를 2진수로 변환
+binary_number = bin(decimal_number)
+print(f"{decimal_number} in binary: {binary_number}")
+
+# 10진수를 8진수로 변환
+octal_number = oct(decimal_number)
+print(f"{decimal_number} in octal: {octal_number}")
+
+# 10진수를 16진수로 변환
+hexadecimal_number = hex(decimal_number)
+print(f"{decimal_number} in hexadecimal: {hexadecimal_number}")
